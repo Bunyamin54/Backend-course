787b67986fc2e66bc6e4618db506cb402216f5e4
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-02 23:04:31 +0100

    aa

---
+++ 01-ExpressJS/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//
+/* -----------------------------------------
+
+                 EXPRESS - JS
+
+----------------------------------------*/
+
+/* 
+
+
+* npm init -y
+* npm i express dotenev   -- seklinde de birden fazla modulu yanyana yazabiliriz.
+* npm i dotenv  env dosyasindan modulu okuma icin 
+
+
+*/
+
